50b7011b3e8faa5b2030f2876ab86cf562d43a12
Aley Amin <aley.amin.22@gmail.com>
2024-04-29 05:58:17 +0300

    Delete functionality

--- GUI.py
+++ GUI.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 from tkinter import ttk
 from tkinter import messagebox
-from ttkbootstrap import Window
+from ttkbootstrap import Window,Button
 from Connect_Database import *
 
 cursor, conn = connect_to_database(DRIVER, SERVER, DATABASE)
@@ -64,10 +64,14 @@ class ManagerPage:
     def __init__(self):
         self.no_of_Emps = tk.StringVar()
         self.no_of_Orders = tk.StringVar()
+        self.deleteobj = None
         self.managerId = login_page.id_var.get()
         self.content = tk.Frame(main_window)
         self.createWidgets()
     def createWidgets(self):
+        self.frame_buttons = tk.Frame(self.content)
+        self.frame_buttons.pack(side='bottom',expand=True,fill='both')
+
         self.frame_employeestable = tk.Frame(self.content)
         self.frame_employeestable.pack(side='bottom',expand=True,fill='both')
 
@@ -86,17 +90,64 @@ class ManagerPage:
         self.label_noofoorders.pack(expand=True,pady=15)
 
         employees = self.getEmps()
-        emp_Table = ttk.Treeview(self.frame_employeestable, columns=('Name', 'Id','sex','Job'),show='headings')
-        emp_Table.heading('Name', text='Name')
-        emp_Table.heading('Id', text='Id')
-        emp_Table.heading('sex', text='Sex')
-        emp_Table.heading('Job', text='Job')
+        self.emp_Table = ttk.Treeview(self.frame_employeestable, columns=('Name', 'Id','sex','Job'),show='headings')
+        self.emp_Table.heading('Name', text='Name')
+        self.emp_Table.heading('Id', text='Id')
+        self.emp_Table.heading('sex', text='Sex')
+        self.emp_Table.heading('Job', text='Job')
 
         for emp in employees:
-            emp_Table.insert(parent='',index=tk.END,values=emp)
+             self.emp_Table.insert(parent='',index=tk.END,values=emp)
 
-        emp_Table.pack(expand=True)
+        self.emp_Table.bind('<<TreeviewSelect>>', self.item_select)
+        self.emp_Table.bind('<Delete>', self.delete_item)
 
+        self.emp_Table.pack(expand=True)
+
+        self.insertEmp = Button(self.frame_buttons,text = "Add Employee" , bootstyle = 'primary')
+        self.insertEmp.pack(side='left',expand=True)
+        self.editEmp = Button(self.frame_buttons,text = "Edit Employee" , bootstyle = 'warning')
+        self.editEmp.pack(side='left',expand=True)
+        self.deleteEmp = Button(self.frame_buttons,text = "Delete Employee" , bootstyle = 'danger' , command= self.deleteEmpbyid)
+        self.deleteEmp.pack(side='left',expand=True)
+
+    def refresh(self):
+        self.emp_Table.pack_forget()
+        self.label_noofemps.pack_forget()
+        employees = self.getEmps()
+        self.no_of_Emps = self.getNoofEmps()
+        self.label_noofemps = tk.Label(self.frame_noofemps,text=f'No Of Employees:\n {self.no_of_Emps}',justify="center",font=("Arial ", 18))
+        self.label_noofemps.pack(expand=True,pady=15)
+        self.emp_Table = ttk.Treeview(self.frame_employeestable, columns=('Name', 'Id','sex','Job'),show='headings')
+        self.emp_Table.heading('Name', text='Name')
+        self.emp_Table.heading('Id', text='Id')
+        self.emp_Table.heading('sex', text='Sex')
+        self.emp_Table.heading('Job', text='Job')
+
+        for emp in employees:
+             self.emp_Table.insert(parent='',index=tk.END,values=emp)
+
+        self.emp_Table.bind('<<TreeviewSelect>>', self.item_select)
+        self.emp_Table.bind('<Delete>', self.delete_item)
+
+        self.emp_Table.pack(expand=True)
+
+
+    def item_select(self,_):
+        self.deleteobj = self.emp_Table.item(self.emp_Table.selection())['values']
+        print(self.deleteobj[1])
+
+    def delete_item(self,_):
+        self.emp_Table.item(self.emp_Table.selection())
+        
+    def deleteEmpbyid(self):
+        id = self.deleteobj[1]
+        print(id)
+        cursor.execute("exec RemoveEmployeeById ?", (id,))
+        cursor.commit()
+        self.refresh()
+        
+        
     def getNoofEmps(self):
         cursor.execute("exec View_Branch_NoOfEmployee ?", (self.managerId,))
         res = cursor.fetchall()
@@ -121,34 +172,83 @@ class SupervisorPage:
     def __init__(self):
         self.no_of_Emps = tk.StringVar()
         self.no_of_Orders = tk.StringVar()
+        self.deleteobj = None
         self.supervisorId = login_page.id_var.get()
         self.content = tk.Frame(main_window)
         self.createWidgets()
     def createWidgets(self):
+        self.frame_buttons = tk.Frame(self.content)
+        self.frame_buttons.pack(side='bottom',expand=True,fill='both')
+
         self.frame_employeestable = tk.Frame(self.content)
         self.frame_employeestable.pack(side='bottom',expand=True,fill='both')
 
-        self.frame_noofoorders = tk.Frame(self.content)
-        self.frame_noofoorders.pack(side='left',expand=True,fill='both')
-
         self.frame_noofemps = tk.Frame(self.content)
-        self.frame_noofemps.pack(side='left',expand=True,fill='both')
+        self.frame_noofemps.pack(side='top',expand=True,fill='both')
 
         self.no_of_Emps = self.getNoofEmps()
         self.label_noofemps = tk.Label(self.frame_noofemps,text=f'No Of Employees:\n {self.no_of_Emps}',justify="center",font=("Arial ", 18))
         self.label_noofemps.pack(expand=True,pady=15)
 
         employees = self.getEmps()
-        emp_Table = ttk.Treeview(self.frame_employeestable, columns=('Name', 'Id','sex','Job'),show='headings')
-        emp_Table.heading('Name', text='Name')
-        emp_Table.heading('Id', text='Id')
-        emp_Table.heading('sex', text='Sex')
-        emp_Table.heading('Job', text='Job')
+        self.emp_Table = ttk.Treeview(self.frame_employeestable, columns=('Name', 'Id','sex','Job'),show='headings')
+        self.emp_Table.heading('Name', text='Name')
+        self.emp_Table.heading('Id', text='Id')
+        self.emp_Table.heading('sex', text='Sex')
+        self.emp_Table.heading('Job', text='Job')
 
         for emp in employees:
-            emp_Table.insert(parent='',index=tk.END,values=emp)
+             self.emp_Table.insert(parent='',index=tk.END,values=emp)
 
-        emp_Table.pack(expand=True)
+        self.emp_Table.pack(expand=True)
+
+        self.insertEmp = Button(self.frame_buttons,text = "Add Employee" , bootstyle = 'primary')
+        self.insertEmp.pack(side='left',expand=True)
+        self.editEmp = Button(self.frame_buttons,text = "Edit Employee" , bootstyle = 'warning')
+        self.editEmp.pack(side='left',expand=True)
+        self.deleteEmp = Button(self.frame_buttons,text = "Delete Employee" , bootstyle = 'danger',  command= self.deleteEmpbyid)
+        self.deleteEmp.pack(side='left',expand=True)
+
+        self.emp_Table.bind('<<TreeviewSelect>>', self.item_select)
+        self.emp_Table.bind('<Delete>', self.delete_item)
+
+        self.emp_Table.pack(expand=True)
+
+    def refresh(self):
+        self.emp_Table.pack_forget()
+        self.label_noofemps.pack_forget()
+        employees = self.getEmps()
+        self.no_of_Emps = self.getNoofEmps()
+        self.label_noofemps = tk.Label(self.frame_noofemps,text=f'No Of Employees:\n {self.no_of_Emps}',justify="center",font=("Arial ", 18))
+        self.label_noofemps.pack(expand=True,pady=15)
+        self.emp_Table = ttk.Treeview(self.frame_employeestable, columns=('Name', 'Id','sex','Job'),show='headings')
+        self.emp_Table.heading('Name', text='Name')
+        self.emp_Table.heading('Id', text='Id')
+        self.emp_Table.heading('sex', text='Sex')
+        self.emp_Table.heading('Job', text='Job')
+
+        for emp in employees:
+             self.emp_Table.insert(parent='',index=tk.END,values=emp)
+
+        self.emp_Table.bind('<<TreeviewSelect>>', self.item_select)
+        self.emp_Table.bind('<Delete>', self.delete_item)
+
+        self.emp_Table.pack(expand=True)
+
+
+    def item_select(self,_):
+        self.deleteobj = self.emp_Table.item(self.emp_Table.selection())['values']
+        print(self.deleteobj[1])
+
+    def delete_item(self,_):
+        self.emp_Table.item(self.emp_Table.selection())
+        
+    def deleteEmpbyid(self):
+        id = self.deleteobj[1]
+        print(id)
+        cursor.execute("exec RemoveEmployeeById ?", (id,))
+        cursor.commit()
+        self.refresh()
 
     def getNoofEmps(self):
         cursor.execute("exec view_NoOfEmployees ?", (self.supervisorId,))
